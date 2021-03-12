2eafa5b190ff20744b7c70543f6b784a2c3a80c2
happyBeagle <mj.jo.sw@gmail.com>
2021-03-12 17:26:15 +0000

    [add file] model_MF.py

---
+++ model/model_MF.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class MF_model(nn.module):
+    def __init__(self, num_users, nums_items, emb_size = 100):
+        super(MF_model, self).__init__()
+        self.users_emb = nn.Embedding(num_users, emb_size)
+        self.items_emb = nn.Embedding(num_items, emb_size)
+        self.users_emb.weight.data.uniform_(0, 0.5)
+        self.items_emb.weight.data.uniform_(0, 0.5)
+
+    def forward(self, u, v):
+        u = self.users_emb(u)
+        v = self.items_emb(v)
+
+        return (u*v).sum(1)

432ff53acdedaeaad914e4f2996bb5c0a21f18b3
happyBeagle <mj.jo.sw@gmail.com>
2021-03-12 17:27:22 +0000

    [add file] utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np
+import os 
+import sys
+from tqdm import tqdm
+from collections import defaultdict  
+
+
+def check_file(path:str, file_type:str, call_func:str)->bool:
+    
+    if not os.path.exists(path):
+        print(f"[Error] {call_func} : file do not exist ==> file name : {path}")
+        return False
+    
+    f_name, f_type = os.path.splitext(path)
+    if f_type is not file_type:
+        print(f"[Error] {call_func} : file type is not {file_type} ==> file type :{f_type}")
+        return False
+    return True

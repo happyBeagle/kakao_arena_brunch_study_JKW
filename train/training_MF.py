2425ced92ee809a2e2931b4aaf938f56be241fb2
happyBeagle <mj.jo.sw@gmail.com>
2021-03-13 04:15:36 +0000

    [modify file] train/training_MF.py : realize recommand function

--- train/training_MF.py
+++ train/training_MF.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import model_MF
+from model import model_MF
 import data_process as dp
 from torch.autograd import Variable  
 
@@ -104,9 +104,59 @@ class Train_MF():
         return predict
 
 
+def get_personal_recommand(dp, model, user_id, recommand_num = 100):
+    recommand_writer_list = model.get_recommend_item(user_id)
+    letter_by_writer = dp.letter_by_writer
+    user_origin_read = dp.get_user_origin_read(user_id)
+
+    recommand_list = []
+    recommand_count = 0
+    for writer in recommand_list:
+        for letter in letter_by_writer[writer]:
+            if letter in user_origin_read:
+                continue
+            
+            recommand_list.append(letter)
+            recommand_count += 1
+
+            if recommand_count >= recommand_num:
+                break
+        if recommand_count >= recommand_num:
+            break
+
+    return recommand_list
+
+
+def recommand_letters(dir_path:str, split_read_data:int, target_user_id_list,  epoch = 100, emb_size = 100, lr = 1e-2, weight_decay = 0.0, if_cuda = False, recommand_num = 100):
     
-def recommand_letters():
+    data_process = dp.Data_processing(dir_path)
+
+    data_process.set_MF_model_data(weight_followee = 2)
+
+    user_data = data_process.user_to_index
+    item_data = data_process.writer_to_index
+
+    rating_train_data = data_process.train_rating_table
+    rating_test_data = data_process.test_rating_table
+
+
+    train_MF = Train_MF(epoch=epoch, emb_size=emb_size, learning_rate=lr, weight_decay=weight_decay, if_cuda=if_cuda)
     
-    train_MF = Train_MF(if_cuda=True)
-    #TODO : 함수 채우기
-         
+    train_MF.set_util_data(user_data, item_data)
+    train_MF.set_train_data(rating_train_data)
+    train_MF.sef_test_data(rating_test_data)
+
+    train_MF.set_model()
+    train_MF.train_model()
+    train_MF.test_model()
+
+    recommand_total_list = []
+    for user_id in target_user_id_list:
+        temp_recommand = [user_id]
+        temp_recommand.extend(get_personal_recommand(dp, model, user_id, recommand_num))
+        recommand_total_list.append(temp_recommand)
+
+    return recommand_total_list
+
+
+if __name__=="__main__":
